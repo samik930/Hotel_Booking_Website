f884773d94c99acec509c7651655f21dc24ae745
samik930 <samikpodder396@gmail.com>
2026-04-23 01:41:51 +0530

    Add Render deployment configuration and environment setup

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,7 @@ connectCloudinary()
 const app = express() 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://hotel-booking-website.onrender.com'],
   credentials: true
 })) //Enable cross origin resource sharing 
 app.use(express.json())
